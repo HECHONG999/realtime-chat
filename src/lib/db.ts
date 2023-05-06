8a92791dc28a2a9e66d95af112c63718938ddf14
HECHONG1101 <hechong_999@163.com>
2023-05-06 23:13:51 +0800

    修复redis startwidth undefine

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,6 @@
-import {Redis} from  '@upstash/redis'
+import { Redis } from '@upstash/redis'
 
 export const db = new Redis({
-    // @ts-ignore
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN
+  url: 'https://apn1-proper-marmot-33806.upstash.io',
+  token: 'AYQOASQgZjEwMmM0YzktZGUyNy00OGQwLWI4OWUtMmY2MDM3MjcyZjQ4NjFjMWY1NGIzM2I2NDQxZWI2MjEyMzAxODMxYTAzZWM=',
 })
