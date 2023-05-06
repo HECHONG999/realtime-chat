97b1840f4e980bf9bd9192aa19ffb5e1e4f5340d
HECHONG1101 <hechong_999@163.com>
2023-05-06 22:54:35 +0800

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react'
+import Button from '@/components/Button'
+interface pageProps {
+
+}
+const page:FC<pageProps> = ({}) => {
+    return <div>
+        <Button 
+        />
+    </div>
+}
+
+export default page
---
+++ src/lib/auth.ts
@@ -0,0 +1,66 @@
+import { NextAuthOptions } from "next-auth";
+import {UpstashRedisAdapter} from '@next-auth/upstash-redis-adapter'
+import { db } from "./db";
+import  GoogleProvider from 'next-auth/providers/google'
+export const authOptions: NextAuthOptions = {
+    adapter: UpstashRedisAdapter(db),
+    session: {
+        strategy: 'jwt'
+    },
+    secret:'111',
+    pages: {
+        signIn: '/login'
+    },
+    providers: [
+        GoogleProvider({
+            clientId: '760503475231-m3njpenar6dpg1apjqh7lo4qfbgecdoo.apps.googleusercontent.com',
+            clientSecret: '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',
+            httpOptions: {
+                timeout: 40000,
+              }
+        })
+    ],
+    callbacks: {
+        async jwt ({token, user}) {
+            console.log(token, user)
+            const dbUser = (await db.get(`user:${token.id}`)) as User | null;
+            if(!dbUser) {
+                token.id = user!.id;
+                return token;
+            }
+            return {
+                id: dbUser.id,
+                name: dbUser.name,
+                email: dbUser.email,
+                picture: dbUser.image
+            }
+        },
+        async session({session, token}) {
+            console.log('session==', session)
+            if(token) {
+                session.user.id  = token.id;
+                session.user.email = token.email;
+                session.user.name = token.name;
+                session.user.image = token.picture
+            }
+            return session;
+        }
+    }
+}
+
+function getGoogleCredentials () {
+    const clientId = process.env.GOOGLE_CLIENT_ID;
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+    console.log('client', clientId)
+    if(!clientId || clientId.length === 0) {
+        throw new Error('Missing GOOGLE_CLIENT_ID')
+    }
+    if(!clientSecret || clientSecret.length === 0) {
+        throw new Error('Missing GOOGLE_CLIENT_SECRET')
+    }
+    debugger
+    return {
+        clientId,
+        clientSecret
+    }
+}
