ecb3d055b7f2ecd4e5d07e08c74ee6b58e6b0edb
hechong <chong@intrii.com>
2023-09-12 21:43:59 +0800

    update env

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,21 +5,6 @@ import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from 'next-auth/providers/github'
 import { fetchRedis } from '@/helpers/redis'
 
-function getGoogleCredentials() {
-  const clientId = process.env.GOOGLE_CLIENT_ID
-  const clientSecret = process.env.GOOGLE_CLIENT_SECRET
-
-  if (!clientId || clientId.length === 0) {
-    throw new Error('Missing GOOGLE_CLIENT_ID')
-  }
-
-  if (!clientSecret || clientSecret.length === 0) {
-    throw new Error('Missing GOOGLE_CLIENT_SECRET')
-  }
-
-  return { clientId, clientSecret }
-}
-
 export const authOptions: NextAuthOptions = {
   adapter: UpstashRedisAdapter(db),
   session: {
